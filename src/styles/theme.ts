31bb13b4408ca67f69850841e2a0351f04ad5af0
n2ptune <n2ptune@outlook.com>
2021-03-16 21:34:04 +0900

    chore: 테스트 주석 제거

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -62,27 +62,6 @@ const margins: Units = {
 
 const palette: Colors = {
   ...colors
-  // green: {
-  //   main: '#5cc76e',
-  //   0: '#e9f2ea',
-  //   1: '#addbb5',
-  //   2: '#8dd899',
-  //   3: '#69db7c',
-  //   4: '#5cc76e',
-  //   5: '#53b463',
-  //   6: '#519b5d',
-  //   7: '#508558',
-  //   8: '#4c7353',
-  //   9: '#48644d'
-  // },
-  // white: {
-  //   main: '#ffffff'
-  // },
-  // grey: {
-  //   main: '#ced4da',
-  //   0: '#ced4da',
-  //   1: '#bcc0c4'
-  // }
 }
 
 const theme: DefaultTheme = {
