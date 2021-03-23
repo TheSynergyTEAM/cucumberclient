688112d88165f5c7f0eeed00a6ab2be3d7bb96cc
dohye1 <tsc03018@gmail.com>
2021-03-23 22:51:48 +0900

    feat: profile에서 보여줄 데이터타입을 state로 관리

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,22 +1,25 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import { RouteComponentProps } from 'react-router'
-import ProfileContainer from 'components/profile/ProfileContainer'
 import ProfileBox from 'components/profile/ProfileBox'
-import UsageInfo from 'components/profile/UsageInfo'
+import ListBox from 'components/profile/ListBox'
 import { Col, Row } from 'antd'
 
 const SignUp: React.FC<RouteComponentProps> = () => {
+  const [selected, setSelected] = useState<string>('chat') // my, trade, like, chat
+
+  const handleChange = useCallback((type: string) => {
+    console.log(type)
+    setSelected(type)
+  }, [])
+
   return (
-    <ProfileContainer>
-      <Row gutter={20}>
-        <Col xs={24} md={8}>
-          <ProfileBox />
-        </Col>
-        <Col xs={24} md={16}>
-          <UsageInfo />
-        </Col>
-      </Row>
-    </ProfileContainer>
+    <Row gutter={20}>
+      <Col xs={24} md={8}>
+        <ProfileBox handleChange={handleChange} />
+        <ListBox selected={selected} handleChange={handleChange} />
+      </Col>
+      <Col xs={24} md={16}></Col>
+    </Row>
   )
 }
 
