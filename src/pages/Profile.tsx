d5523c7a7f1bcb000bd2e9fbdc6a14c163f88152
hyewon <tsc03018@gmail.com>
2021-03-25 16:47:26 +0900

    feat: 각 항목에 맞는 처리 해주기

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,23 +2,50 @@ import React, { useState, useCallback } from 'react'
 import { RouteComponentProps } from 'react-router'
 import ProfileBox from 'components/profile/ProfileBox'
 import ListBox from 'components/profile/ListBox'
+import EditProfile from 'components/profile/EditProfile'
+import Trade from 'components/profile/Trade'
+import Like from 'components/profile/Like'
 import { Col, Row } from 'antd'
+import { ArticleBoxProps } from 'components/common/ArticleBox'
+
+// test data
+const article: ArticleBoxProps = {
+  img:
+    'https://dnvefa72aowie.cloudfront.net/origin/article/202103/E1F4AA664E524FD622535207EEB9A445165DB89079455111AD4E48433DDB20F5.jpg?q=82&s=300x300&t=crop',
+  name: '자전거팝니다',
+  price: '100,000',
+  region: '서울 중구',
+  like: 3,
+  chat: 2
+}
 
 const SignUp: React.FC<RouteComponentProps> = () => {
-  const [selected, setSelected] = useState<string>('chat') // my, trade, like, chat
+  const [selected, setSelected] = useState<string>('my') // my, trade, like, chat
+
+  const selectedContent = useCallback(() => {
+    switch (selected) {
+      case 'my':
+        return <EditProfile />
+      case 'trade':
+        return <Trade buyList={[article]} sellList={[]} />
+      case 'like':
+        return <Like likeList={[article, article]} />
+    }
+  }, [selected])
 
   const handleChange = useCallback((type: string) => {
-    console.log(type)
     setSelected(type)
   }, [])
 
   return (
     <Row gutter={20}>
       <Col xs={24} md={8}>
-        <ProfileBox handleChange={handleChange} />
+        <ProfileBox />
         <ListBox selected={selected} handleChange={handleChange} />
       </Col>
-      <Col xs={24} md={16}></Col>
+      <Col xs={24} md={16}>
+        {selectedContent()}
+      </Col>
     </Row>
   )
 }
