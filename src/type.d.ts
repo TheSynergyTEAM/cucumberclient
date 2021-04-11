63cae70f037f8d143312494bcad2d7507b3ce4f9
hyewon do <hyewondo@hyewonui-MacBookPro.local>
2021-04-11 14:45:29 +0900

    feat: detail page 구성

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,11 +1,28 @@
 /* eslint-disable */
 declare namespace article {
   interface ArticleInfo {
+    id?: number
     img: string
     name: string
     price: string
     region: string
     like: number
     chat: number
+  }
+  interface ArticleDetail {
+    id: number
+    img: string[]
+    nickname: string
+    rating: number
+    name: string
+    category: string
+    upload_time: string
+    price: string
+    region: string
+    like: number
+    chat: number
+    views: number
+    user_profile: string
+    desc: string
   }
 }
