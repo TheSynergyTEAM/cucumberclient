3be7a672d34f482ff7e21d346dbf074b50523051
n2ptune@outlook.com <n2ptune@outlook.com>
2021-04-13 14:03:07 +0900

    feat: axios interceptor 정의 및 토큰 localStorage에 저장하기

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,3 +1,21 @@
-// import axios from 'axios'
+import axios from 'axios'
 
-export default {}
+type LoginUser = {
+  email: string
+  password: string
+}
+
+// 로그인
+export const login: (user: LoginUser) => Promise<User> = async (user) => {
+  try {
+    const { data, headers } = await axios.post('/login', {
+      email: user.email,
+      password: user.password
+    })
+    // 로컬 스토리지에 토큰 등록
+    localStorage.setItem('token', headers.authorization)
+    return data
+  } catch (error) {
+    throw error.response.data
+  }
+}
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -6,6 +6,12 @@ export default function createAxiosInstance(): void {
 
   // 요청 혹은 응답을 가로채서 어떤 행위를 할 수 있는 인터셉터를 정의합니다.
   // 참고: https://xn--xy1bk56a.run/axios/guide/interceptors.html
-  // axios.interceptors.request.use()
+  axios.interceptors.request.use((value) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      value.headers.Authorization = token
+    }
+    return value
+  })
   // axios.interceptors.response.use()
 }
