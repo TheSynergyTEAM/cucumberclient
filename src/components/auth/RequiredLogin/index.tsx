ff4405f0c5a6ff8661730426e6a0536852ef19a6
n2ptune@outlook.com <n2ptune@outlook.com>
2021-04-29 20:52:22 +0900

    fix: hoc typing

--- src/components/auth/RequiredLogin/index.tsx
+++ src/components/auth/RequiredLogin/index.tsx
@@ -1,20 +1,27 @@
+/* eslint-disable */
 import { message } from 'antd'
 import userContext from 'context/user'
 import React, { useContext, useEffect } from 'react'
 import { useHistory } from 'react-router'
 
-export default function withRequiredLogin(
-  Component: React.ComponentType<any>
-): React.ComponentType<any> {
-  const { isLoggedIn } = useContext(userContext)
-  const history = useHistory()
+const withRequiredLogin: (Component: React.ComponentType<any>) => React.FC = (
+  Component
+) => {
+  const Wrapper: React.FC = () => {
+    const { isLoggedIn } = useContext(userContext)
+    const history = useHistory()
 
-  useEffect(() => {
-    if (!isLoggedIn) {
-      history.push('/')
-      message.error({ content: '해당 페이지는 로그인이 필요합니다.' })
-    }
-  }, [])
+    useEffect(() => {
+      if (!isLoggedIn) {
+        history.push('/')
+        message.error({ content: '해당 페이지는 로그인이 필요합니다.' })
+      }
+    }, [])
 
-  return Component
+    return <Component />
+  }
+
+  return Wrapper
 }
+
+export default withRequiredLogin
